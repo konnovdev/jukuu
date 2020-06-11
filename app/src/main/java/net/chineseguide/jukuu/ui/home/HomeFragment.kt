924e427a633c4a201f765931210b50ec313dc68e
konnovdev <konnovdev@gmail.com>
2020-06-11 19:11:43 +0700

    Added parsing results

--- app/src/main/java/net/chineseguide/jukuu/ui/home/HomeFragment.kt
+++ app/src/main/java/net/chineseguide/jukuu/ui/home/HomeFragment.kt
@@ -9,6 +9,8 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
 import net.chineseguide.jukuu.databinding.FragmentHomeBinding
 import net.chineseguide.jukuu.di.viewModel
 import net.chineseguide.jukuu.domain.entity.Sentence
@@ -23,7 +25,11 @@ class HomeFragment : Fragment() {
 
     private lateinit var adapter: SentencesAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentHomeBinding.inflate(inflater)
         return binding.root
     }
@@ -66,14 +72,38 @@ class HomeFragment : Fragment() {
     private fun renderState(state: HomeState) {
         when (state) {
             is HomeState.Progress -> {
-                binding.progressBar.isVisible = true
+                showInProgress()
             }
             is HomeState.Success -> {
-                binding.progressBar.isVisible = false
-                adapter.itemList = state.sentenceCollection.sentences
-                adapter.notifyDataSetChanged()
+                showContent(state.sentenceCollection.sentences)
+
             }
-            else -> throw IllegalArgumentException("unhandled state $state")
+            is HomeState.EmptyResult -> {
+                showContent(emptyList())
+                Snackbar.make(binding.parentLayout, "Nothing was found!", LENGTH_SHORT).show()
+            }
+            is HomeState.Error -> {
+                showContent(emptyList())
+                Snackbar.make(
+                    binding.parentLayout,
+                    "Error occurred! " + { state.exception } + ". Please report to konnovdev@gmail.com",
+                    LENGTH_SHORT
+                ).show()
+            }
         }
+    }
+
+    private fun showInProgress() {
+        binding.searchButton.isEnabled = false
+        binding.searchBar.isEnabled = false
+        binding.progressBar.isVisible = true
+    }
+
+    private fun showContent(sentences: List<Sentence>) {
+        binding.searchButton.isEnabled = true
+        binding.searchBar.isEnabled = true
+        binding.progressBar.isVisible = false
+        adapter.itemList = sentences
+        adapter.notifyDataSetChanged()
     }
 }
