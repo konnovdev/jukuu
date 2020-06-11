82c3b0219b0630ea5386d3ca737b9531a3472d4f
konnovdev <konnovdev@gmail.com>
2020-06-11 20:03:48 +0700

    Deleted `Details` screen for now
    Most likely i will NOT need it

--- app/src/main/java/net/chineseguide/jukuu/ui/arguments.kt
+++
@@ -1,9 +0,0 @@
-package net.chineseguide.jukuu.ui
-
-import android.os.Bundle
-
-private const val TASK_ID_ARG = "task_id_arg"
-
-var Bundle?.taskId: String
-    get() = this?.getString(TASK_ID_ARG)!!
-    set(value) = this!!.putString(TASK_ID_ARG, value)
