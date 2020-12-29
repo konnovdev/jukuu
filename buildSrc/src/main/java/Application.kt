c2604dc00505f80a1471821823d53b499beb9fe5
konnovdev <konnovdev@gmail.com>
2020-12-29 23:29:51 +0700

    Fixed wrong application id
    buildSrc was copied from a different project and i forgot to change `id` of the application

--- buildSrc/src/main/java/Application.kt
+++ buildSrc/src/main/java/Application.kt
@@ -1,5 +1,5 @@
 object Application {
-    const val id = "konnov.i.maydurov.architecture.flex"
+    const val id = "net.chineseguide.jukuu"
     const val minApi = 19
     const val maxApi = 28 // TODO upgrade to 30
     const val versionCode = 1
